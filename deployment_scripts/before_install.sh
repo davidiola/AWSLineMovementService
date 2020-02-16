25f3247ece98885b2daaa06f476104c127a9cba5
David Iola <iola2@illinois.edu>
2020-02-15 17:03:49 -0800

    add cronjob to run prog

--- deployment_scripts/before_install.sh
+++ deployment_scripts/before_install.sh
@@ -2,3 +2,9 @@
 
 yes | sudo yum install java-1.8.0-openjdk-devel # includes compiler
 yes | sudo yum remove java-1.7.0-openjdk
+
+# add cron job to run prog every 5 min
+crontab -l > mycron
+echo "*/5 * * * * cd /home/ec2-user/; sudo ./gradlew run" >> mycron
+crontab mycron
+rm mycron
