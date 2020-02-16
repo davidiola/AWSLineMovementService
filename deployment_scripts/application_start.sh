411008603df26a7403ab79d08e89dd43b2a223c3
David Iola <iola2@illinois.edu>
2020-02-15 16:19:52 -0800

    use java package with compiler

--- deployment_scripts/application_start.sh
+++ deployment_scripts/application_start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-pwd
 cd /home/ec2-user
-./gradlew run
+sudo ./gradlew run
--- deployment_scripts/before_install.sh
+++ deployment_scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-yes | sudo yum install java-1.8.0
+yes | sudo yum install java-1.8.0-openjdk-devel # includes compiler
 yes | sudo yum remove java-1.7.0-openjdk
